a174037d5c149039b58665411f405c7300bb6881
DaveSamiaji <89759815+DaveSamiaji@users.noreply.github.com>
2021-12-13 04:46:00 +0700

    Delete project_1.py

--- chapter11/project_1.py
+++
@@ -1,8 +0,0 @@
-from datetime import *
-def diffDate(x):
-    today = datetime.date(datetime.now())
-    y, m, d = x.split('-')
-    tglCari = date(int(y), int(m), int(d))
-    selisih = today - tglCari
-    selisihHari= abs(selisih.days)
-    return selisihHari
